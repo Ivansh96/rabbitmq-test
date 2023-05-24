8d48bc06c3e5cfc0a8ff00dc60766d1d19a8bc9e
Ivansh96 <evgenijshavshin@yandex.ru>
2023-05-25 01:46:27 +0700

    -

---
+++ src/main/java/com/rabbitmqtest/dto/UserDto.java
@@ -0,0 +1,10 @@
+package com.rabbitmqtest.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private Integer id;
+    private String firstname;
+    private String lastname;
+}
---
+++ src/main/java/com/rabbitmqtest/publisher/RabbitMQJsonProducer.java
@@ -0,0 +1,28 @@
+package com.rabbitmqtest.publisher;
+
+import com.rabbitmqtest.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RabbitMQJsonProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQJsonProducer.class);
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.json.key}")
+    private String routingJsonKey;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void sendJsonMessage(UserDto user) {
+        LOGGER.info(String.format("Json message send -> %s", user.toString()));
+        rabbitTemplate.convertAndSend(exchange, routingJsonKey, user);
+    }
+}
